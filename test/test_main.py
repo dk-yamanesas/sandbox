d618337b42557152f4dcb092020d1a0447b7406f
yamanesas <sasuke.yamane@daikin.co.jp>
2022-04-27 11:57:45 +0900

    add new test

--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,20 @@
 import pytest
-from src.main import calc
+from src.main import add, sub
 
 
 @pytest.mark.parametrize("params", [(1, 1)])
-def test(params):
+def test_add(params):
     x = params[0]
     y = params[1]
-    ans = calc(x, y)
+    ans = add(x, y)
 
     assert ans == x + y
+
+
+@pytest.mark.parametrize("params", [(1, 1)])
+def test_sub(params):
+    x = params[0]
+    y = params[1]
+    ans = sub(x, y)
+
+    assert ans == x - y
